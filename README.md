9e4510ff7e72b44732fa6a3c8d79e94952465481
KhaledAqrainy <khaledalqrainy@gmail.com>
2021-10-16 03:28:15 +0300

    fix tests

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-PR link : https://github.com/KhaledAlqrainy/pythonic-garage-band/pull/1
+# Classes, and Fixtures
+
+### Problem Domain
+
+* create two classes for band and musician and and having instances to call methods and test the result.
+
+
+ **PR link** : https://github.com/KhaledAlqrainy/pythonic-garage-band/pull/1
--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -27,32 +27,37 @@ class Band:
 
 class Musician:
 
-    def __init__(self, name, instrument):
+    def __init__(self, name):
         self.name = name
-        self.instrument = instrument
-
+        
     def __repr__(self):
-        return f"{self.__class__.__name__} instance. Name : {self.name}"    
+        pass   
 
     def __str__(self):
-        return f"My name is {self.name} , and I play {self.instrument}"
+        pass
 
 
 ########## Musician Types ###########
 
 class Guitarist(Musician):
-    def __init__(self, name):
-        super().__init__(name, "guitar")  
+
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
+
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}" 
 
     def get_instrument(self):
-        return self.instrument
+        return 'guitar'
 
     def play_solo(self):
         return "face melting guitar solo"
 
 class Bassist(Musician):
-    def __init__(self, name):    
-        super().__init__(name, "bass")
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
 
     def get_instrument(self):
         return "bass"
@@ -61,11 +66,23 @@ class Bassist(Musician):
         return "bom bom buh bom"
 
 class Drummer(Musician):
-    def __init__(self, name):
-        super().__init__(name, "drums")
+
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
 
     def get_instrument(self):
         return "drums"
 
     def play_solo(self):
         return "rattle boom crash"
+
+if __name__ == "__main__":
+    Joan = Guitarist('Joan Jett')
+    Sheila = Drummer('Sheila E.')
+    Meshell = Bassist('Meshell Ndegeocello')
+
+    print(Joan)
+    print(Sheila)
+    print(Meshell)
